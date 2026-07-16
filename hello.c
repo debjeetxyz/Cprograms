68387c74ea2d62fc36d74811efe5c13b64096209
debjeetxyz <purakayasthadebjeet@gmail.com>
2026-07-17 01:46:40 +0530

    Hello World

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void HelloWorld(void);
+int main() {
+    //That's how I make simple things complex :) 
+    HelloWorld();
+    return 0;
+}
+void HelloWorld()
+{
+    printf("Hello World.");
+}
